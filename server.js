f054e3db07aa0b6c7c6f286798129c8e3fc27579
Marc Negron <negronmarc@gmail.com>
2022-01-08 21:07:07 -0500

    fixed the routes

--- server.js
+++ server.js
@@ -1,34 +1,16 @@
 const express = require("express");
-const path = require("path");
-const fs = require("fs");
-
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
-
-app.use(express.urlencoded({ extended: true }));
-
 app.use(express.static("public"));
 
-app.get("/api/notes", (req, res) => {
-  const notes = JSON.parse(fs.readFileSync("db/db.json"));
-  res.json(notes);
-});
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
 
-app.post('/api/notes', (req, res) => {
-  console.log("POST /api/notes");
-    
-});
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 
-app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/notes.html"));
+app.listen(PORT, () => {
+  console.log("working");
 });
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/index.html"));
-});
-
-app.listen(PORT, () =>
-  console.log(`Example app listening at http://localhost:${PORT}`)
-);
